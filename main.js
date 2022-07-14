9abf1fb5caa45659b83f22c6583327f57a4b8937
RahulGautamSingh <rahultesnik@gmail.com>
2022-07-14 17:11:05 +0530

    add main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+function addTag() {
+    const tagName = document.getElementById("tagInput").value;
+    console.log(tagName)
+    document.getElementById("tagInput").value = ""
+}
+
+// making half of the letters in a word bold
+function highlightText(sentenceText) {
+    return sentenceText
+        .replace(/\p{L}+/gu, (word) => {
+            const { length } = word;
+            let midPoint = 1;
+            if (length > 3) midPoint = Math.round(length / 2);
+            const firstHalf = word.slice(0, midPoint);
+            const secondHalf = word.slice(midPoint);
+            const htmlWord = '<strong>' + firstHalf + '</strong>' + secondHalf;
+            return htmlWord;
+        });
+}
+
+function convertIt() {
+    const tags = ['p', 'span', 'li']
+    const ignoreChars = ['&'];
+    function convertToBionic(stri) {
+        let upperStr = stri.split(".");
+        upperStr = upperStr.map(str => highlightText(str));
+        return upperStr.join(".")
+    }
+    for (let tag of tags) {
+        const tagS = document.getElementsByTagName(tag);
+        for (let index = 0; index < tagS.length; index++) {
+            tagS[index].innerHTML = convertToBionic(tagS[index].innerText)
+        };
+    }
+}
+
+
+convertIt();
+window.addEventListener('scroll', convertIt);
